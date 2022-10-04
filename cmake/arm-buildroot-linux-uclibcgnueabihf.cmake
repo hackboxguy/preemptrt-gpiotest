909100dabf99f996ceede89e2ecf9da93bd19413
hackboxguy <albert.david@gmail.com>
2022-10-04 07:12:04 +0000

    fixed cmake for correct cross-toolchain path

--- cmake/arm-buildroot-linux-uclibcgnueabihf.cmake
+++ cmake/arm-buildroot-linux-uclibcgnueabihf.cmake
@@ -2,11 +2,11 @@
 SET(CMAKE_SYSTEM_NAME Linux)
 
 # specify the cross compiler
-SET(CMAKE_C_COMPILER   $ENV{HOME}/arm-buildroot-linux-uclibcgnueabihf_sdk-buildroot/usr/bin/arm-buildroot-linux-uclibcgnueabihf-gcc)
-SET(CMAKE_CXX_COMPILER $ENV{HOME}/arm-buildroot-linux-uclibcgnueabihf_sdk-buildroot/usr/bin/arm-buildroot-linux-uclibcgnueabihf-g++)
+SET(CMAKE_C_COMPILER   $ENV{HOME}/arm-buildroot-linux-gnueabihf_sdk-buildroot/usr/bin/arm-buildroot-linux-gnueabihf-gcc)
+SET(CMAKE_CXX_COMPILER $ENV{HOME}/arm-buildroot-linux-gnueabihf_sdk-buildroot/usr/bin/arm-buildroot-linux-gnueabihf-g++)
 
 # where is the target environment 
-SET(CMAKE_FIND_ROOT_PATH  $ENV{HOME}/arm-buildroot-linux-uclibcgnueabihf_sdk-buildroot/arm-buildroot-linux-uclibcgnueabihf/sysroot)
+SET(CMAKE_FIND_ROOT_PATH  $ENV{HOME}/arm-buildroot-linux-gnueabihf_sdk-buildroot/arm-buildroot-linux-gnueabihf/sysroot)
 
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
